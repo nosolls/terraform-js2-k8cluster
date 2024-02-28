c00a0d9efe83825f14f246c023dcbf15c3e27b8b
Noah S. Oller Smith <nollersm@purdue.edu>
2024-02-28 14:48:30 -0600

    Fix issue with GPU node IP

--- output.tf
+++ output.tf
@@ -3,27 +3,27 @@
  ################
 
 
- output "floating_ip_ubuntu20_leader" {
-   value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_leader.*.address
+ output "floating_ip_ubuntu20_leader_" {
+   value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_leader_.*.address
    description = "Public IP for Ubuntu 20 leader"
  }
 
- output "floating_ip_ubuntu20_follower" {
-   value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower.*.address
+ output "floating_ip_ubuntu20_follower_" {
+   value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower_.*.address
    description = "Public IP for Ubuntu 20 followers"
  }
 
- output "floating_ip_ubuntu20_gpu" {
-   value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_gpu.*.address
+ output "floating_ip_ubuntu20_gpu_" {
+   value = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_gpu_.*.address
    description = "Public IP for Ubuntu 20 gpus"
  }
 
  resource "local_file" "ansible_inventory" {
    content = templatefile("${path.module}/inventory.tftpl",
    {
-     ansible_leader_ip    = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_leader.*.address,
-     ansible_follower_ip  = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower.*.address,
-     ansible_gpu_ip	  = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower.*.address,
+     ansible_leader_ip    = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_leader_.*.address,
+     ansible_follower_ip  = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_follower_.*.address,
+     ansible_gpu_ip	  = openstack_networking_floatingip_v2.terraform_floatip_ubuntu20_gpu_.*.address,
    }
    )
    filename  = "ansible/inventory.ini"
